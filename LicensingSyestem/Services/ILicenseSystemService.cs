720057c47197dbbbdeb2feada7c6b16766ff09c2
S M <sm-soft@live.com>
2020-11-13 17:04:31 +0300

    some new functions added to the service

--- LicensingSyestem/Services/ILicenseSystemService.cs
+++ LicensingSyestem/Services/ILicenseSystemService.cs
@@ -8,24 +8,28 @@ namespace LicensingSyestem.Services
 {
     public interface ILicenseSystemService
     {
+
+        Task<List<Application>> GetApplicationListAsync();
+        Task<List<License>> GetAllLicenseListAsync();
+        Task<List<License>> GetLicenseListAsync(int application);
         Task<int> AddNewApplicationAsync(Application application);
         Task<int> AddNewLisenseAsync(License license);
         /// <summary>
-        /// 
+        /// adds new client for license
         /// </summary>
         /// <param name="lisenseCode"></param>
         /// <param name="client"></param>
         /// <returns>0 license not valid, -1 client count is over, >0 success</returns>
         Task<int> AddClientAsync(string lisenseCode, Client client);
         /// <summary>
-        /// 
+        /// adds new client for license, first checks all validations of license
         /// </summary>
         /// <param name="Id"></param>
         /// <param name="client"></param>
         /// <returns>0 license not valid, -1 client count is over, >0 success</returns>
         Task<int> AddClientAsync(Guid Id, Client client);
         Task<License> GetLicenseAsync(string code);
-        Task<bool> IsUserLimitReached(string lisenseCode);
+        Task<bool> IsUserLimitReachedAsync(string lisenseCode);
         Task<bool> CanLoginAsync(string licenseCode, string UniqueId, string DeviceId);
     }
 }
--- LicensingSyestem/Services/LicenseSystemService.cs
+++ LicensingSyestem/Services/LicenseSystemService.cs
@@ -1,6 +1,7 @@
 ﻿using LicensingSyestem.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,16 +10,14 @@ namespace LicensingSyestem.Services
     public class LicenseSystemService : ILicenseSystemService
     {
         private SMDBContext dbContext;
-
         public LicenseSystemService(SMDBContext dbContext)
         {
             this.dbContext = dbContext;
         }
-
         public async Task<int> AddClientAsync(string lisenseCode, Client client)
         {
 
-            var license = await dbContext.Licenses.FirstOrDefaultAsync(q => q.Code == lisenseCode);
+            var license = await dbContext.Licenses.AsNoTracking().FirstOrDefaultAsync(q => q.Code == lisenseCode);
             if (license == null || !license.Active || (license.DeactiveateAfterExpiration && license.ExpirationDate.HasValue && license.ExpirationDate.Value > DateTime.Now))
                 return 0;
 
@@ -29,24 +28,20 @@ namespace LicensingSyestem.Services
             dbContext.Clients.Add(client);
             return await dbContext.SaveChangesAsync();
         }
-
         private async Task<bool> IsUserLimitReached(Guid licenseid, int MaxCount)
         {
-            var IsUserLimited = await dbContext.Clients.CountAsync(q => q.LicenseId.Equals(licenseid)) >= MaxCount;
+            var IsUserLimited = await dbContext.Clients.AsNoTracking().CountAsync(q => q.LicenseId.Equals(licenseid)) >= MaxCount;
             return IsUserLimited;
         }
-
-        public async Task<bool> IsUserLimitReached(string lisenseCode)
+        public async Task<bool> IsUserLimitReachedAsync(string lisenseCode)
         {
-            var license = await dbContext.Licenses.FirstOrDefaultAsync(q => q.Code == lisenseCode);
+            var license = await dbContext.Licenses.AsNoTracking().FirstOrDefaultAsync(q => q.Code == lisenseCode);
             var registedCount = await dbContext.Clients.CountAsync(q => q.LicenseId.Equals(license.Id));
             return registedCount >= license.AllowedClientCount;
         }
-
-
         public async Task<int> AddClientAsync(Guid Id, Client client)
         {
-            var license = await dbContext.Licenses.FirstOrDefaultAsync(q => q.Id == Id);
+            var license = await dbContext.Licenses.AsNoTracking().FirstOrDefaultAsync(q => q.Id == Id);
             if (license == null || !license.Active || (license.DeactiveateAfterExpiration && license.ExpirationDate.HasValue && license.ExpirationDate.Value > DateTime.Now))
                 return 0;
 
@@ -57,31 +52,30 @@ namespace LicensingSyestem.Services
             dbContext.Clients.Add(client);
             return await dbContext.SaveChangesAsync();
         }
-
         public async Task<int> AddNewApplicationAsync(Application application)
         {
             dbContext.Applications.Add(application);
             return await dbContext.SaveChangesAsync();
         }
-
         public async Task<int> AddNewLisenseAsync(License license)
         {
             dbContext.Licenses.Add(license);
             return await dbContext.SaveChangesAsync();
         }
-
         public async Task<bool> CanLoginAsync(string licenseCode, string UniqueId, string DeviceId)
         {
-            var lic = await dbContext.Licenses.FirstOrDefaultAsync(w => w.Code == licenseCode);
+            var lic = await dbContext.Licenses.AsNoTracking().FirstOrDefaultAsync(w => w.Code == licenseCode);
             if (lic == null || !lic.Active) return false;
 
-            var clinet = await dbContext.Clients.FirstOrDefaultAsync(q => q.LicenseId == lic.Id && q.UniqueID == UniqueId && q.DeviceID == DeviceId);
+            var clinet = await dbContext.Clients.AsNoTracking().FirstOrDefaultAsync(q => q.LicenseId == lic.Id && q.UniqueID == UniqueId && q.DeviceID == DeviceId);
 
             if (clinet == null || !clinet.Active) return false;
 
             return true;
         }
-
         public async Task<License> GetLicenseAsync(string code) => await dbContext.Licenses.FirstOrDefaultAsync(w => w.Code == code);
+        public async Task<List<Application>> GetApplicationListAsync() => await dbContext.Applications.AsNoTracking().ToListAsync();
+        public async Task<List<License>> GetAllLicenseListAsync() => await dbContext.Licenses.AsNoTracking().ToListAsync();
+        public async Task<List<License>> GetLicenseListAsync(int application) => await dbContext.Licenses.AsNoTracking().Where(v => v.AppId == application).ToListAsync();
     }
 }
